298c4ebd452e1b38c38e1850996dd8b095cdeafd
Michael Granger <ged@FaerieMUD.org>
2010-03-08 15:56:18 -0800

    First working version of the spike. Now to implement as a library/command-line tool.

--- experiments/sslauthclient-spike.rb
+++ experiments/sslauthclient-spike.rb
@@ -1,5 +1,7 @@
 #!/usr/bin/env ruby
 
+require 'etc'
+
 require 'drb'
 require 'drb/ssl'
 require 'openssl'
@@ -8,6 +10,8 @@ require 'termios'
 
 SERVICE_URI = "drbssl://localhost:8484"
 
+# Don't bother verifying the server's cert -- likely will want to
+# change this to something stricter for real services.
 config = {
 	:SSLVerifyMode => OpenSSL::SSL::VERIFY_NONE
 }
@@ -46,22 +50,39 @@ def prompt_for_password( prompt="Password: " )
 end
 
 
+# Use the name of the current user or the 'USER' environment variable
+username = Etc.getpwuid( Process.euid ).name || ENV['USER']
+pass = prompt_for_password()
+newuser = ARGV.first || username
+
+# Connect to the service
 $stderr.puts "Starting the SSL+DRb service..."
 DRb.start_service( nil, nil, config )
-$stderr.puts "  getting the authenticator object..."
-authenticator = DRbObject.new_with_uri( SERVICE_URI )
-$stderr.puts "  got it: %p" % [ authenticator ]
+$stderr.puts "  getting the authenticated service object..."
+service = DRbObject.new_with_uri( SERVICE_URI )
+$stderr.puts "  got it: %p" % [ service ]
 
-pass = prompt_for_password()
-$stderr.puts "  authenticating..."
-rval = authenticator.authenticate( 'mgranger', pass ) do |service|
-	$stderr.puts "Authenticated. Service is: %p" % [ service ]
-	if service.homedir_exist?( 'mgranger' )
-		$stderr.puts "  homedir exists."
-	else
-		$stderr.puts "  homedir doesn't exist."
-	end
+# Try to call the obscured method before authenticating
+$stderr.puts "  calling the unguarded method before authenticating..."
+if service.homedir_exist?( newuser )
+	$stderr.puts "  homedir exists."
+else
+	$stderr.puts "  homedir doesn't exist."
 end
 
-$stderr.puts "  authenticator returned: %p" % [ rval ]
+
+$stderr.puts "  calling the guarded method before authenticating..."
+begin
+	service.create_homedir( newuser )
+rescue SecurityError => err
+	$stderr.puts "  yep, raised a %s: %s" % [ err.class.name, err.message ]
+end
+
+# Authenticate and call the obscured method if successfullyul.
+$stderr.puts "  authenticating..."
+service.authenticate( username, pass ) do
+	$stderr.puts "Authenticated successfully."
+	msg = service.create_homedir( newuser )
+	$stderr.puts( msg )
+end
 
--- experiments/sslauthservice-spike.rb
+++ experiments/sslauthservice-spike.rb
@@ -7,7 +7,7 @@ require 'openssl'
 
 require 'treequel'
 
-
+COMPANY_NAME = 'laika'
 SERVICE_URI = "drbssl://localhost:8484"
 
 EXPDIR = Pathname( __FILE__ ).dirname
@@ -15,59 +15,116 @@ KEYFILE = EXPDIR + 'key.pem'
 CERTFILE = EXPDIR + 'cert.pem'
 
 SSL_CONFIG     = {
-	:C            => 'US',
-	:ST           => 'Oregon',
-	:L            => 'Portland',
-	:O            => 'LAIKA, Inc.',
-	:CN           => 'services.laika.com',
-	:emailAddress => 'it@lists.laika.com',
+	'C'            => 'US',
+	'ST'           => 'Oregon',
+	'L'            => 'Portland',
+	'O'            => "#{COMPANY_NAME.upcase}, Inc.",
+	'CN'           => "services.#{COMPANY_NAME}.com",
+	'emailAddress' => "it@lists.#{COMPANY_NAME}.com",
 }
 
 class Service
+	include DRbUndumped
 
-	### Create the service object
-	def initialize
-		@homedir = Pathname( '/laika/home' )
+	@@real_methods = {}
+	@unguarded_mode = false
+	class << self; attr_accessor :unguarded_mode; end
+
+	def self::unguarded
+		self.unguarded_mode = true
+		yield
+	ensure
+		self.unguarded_mode = false
 	end
 
-	### Return +true+ if a home directory for the specified +username+ exists.
-	def homedir_exist?( username )
-		userhome = @homedir + username[0,1] + username
-		return username.exist?
+	def self::method_added( meth )
+		super
+		unless self.unguarded_mode || self == ::Service || meth.to_sym == :initialize
+			$stderr.puts "Obscuring #{meth}."
+			@@real_methods[ self ] ||= {}
+			@@real_methods[ self ][ meth.to_sym ] = self.instance_method( meth )
+			remove_method( meth )
+		end
+	end
+
+
+	def self::real_method( sym )
+		return @@real_methods[ self ][ sym ]
+	end
+
+
+	def initialize
+		@ldap = Treequel.directory( "ldap://ldap.#{COMPANY_NAME}.com/dc=#{COMPANY_NAME},dc=com" )
+		@people = @ldap.ou( :people )
+		@authenticated = false
+		super
+	end
+
+	attr_reader :authenticated
+
+	### Authenticate, and if successful, add the service to the proxied object.
+	def authenticate( uid, password )
+		person = @people.uid( uid )
+		$stderr.puts "Checking authentication for %p" % [ person ]
+		@ldap.bound_as( person, password ) do
+			$stderr.puts "Authentication succeeded. Setting authenticated flag."
+			@authenticated = true
+			yield
+		end
+
+	rescue => err
+		$stderr.puts "%s while authenticating %p: %s" % [ err.class.name, uid, err.message ]
+		return nil
+	ensure
+		@authenticated = false
+	end
+
+
+	#########
+	protected
+	#########
+
+	def method_missing( sym, *args )
+		return super unless body = self.class.real_method( sym ).clone
+		raise SecurityError, "not authenticated" unless self.authenticated
+		return body.bind( self ).call( *args )
 	end
 
 end # class Service
 
 
-class AuthWrapperObject
-	include DRbUndumped
+### A simple little example service that checks for the existance of a home directory following 
+### the convention of /#{COMPANY_NAME}/home/<first character of username>/<username>.
+class HomeDirService < Service
 
+	### Create the service object
 	def initialize
-		@ldap = Treequel.directory( 'ldap://ldap.laika.com/dc=laika,dc=com' )
-		@people = @ldap.ou( :people )
-		@serviceobject = nil
+		@homedir = Pathname( "/#{COMPANY_NAME}/home" )
 		super
 	end
 
+	unguarded do
 
-	### Authenticate, and if successful, add the service to the proxied object.
-	def authenticate( uid, password )
-		person = @people.uid( uid )
-		begin
-			@ldap.bound_as( person, password ) do
-				yield Service.new
-			end
-		rescue => err
-			$stderr.puts "%s while authenticating %p: %s" % [ err.class.name, uid, err.message ]
-			return nil
+		### Return +true+ if a home directory for the specified +username+ exists.
+		def homedir_exist?( username )
+			userhome = @homedir + username[0,1] + username
+			$stderr.puts "Checking existance of %p" % [ userhome ]
+			return userhome.exist?
 		end
+
 	end
 
-end # class AuthWrapperObject
+	def create_homedir( username )
+		userhome = @homedir + username[0,1] + username
+		$stderr.puts "Creating homedir for %s: %p" % [ username, userhome ]
+		return  "%s created (well, not really, but it would have been)" % [ userhome ]
+	end
+
+end # class Service
 
 
+### Generate a key if there isn't already one in the current directory.
 cert = key = nil
-
 unless KEYFILE.exist?
 	$stderr.print "Generating server cert..."
 	key = OpenSSL::PKey::RSA.new( 2048 ){ $stderr.print "." }
@@ -78,6 +135,7 @@ unless KEYFILE.exist?
 end
 key  ||= OpenSSL::PKey::RSA.new( KEYFILE.read )
 
+### Same for the cert -- generate one on demand
 unless CERTFILE.exist?
 	name = OpenSSL::X509::Name.new( SSL_CONFIG.to_a )
 
@@ -115,7 +173,8 @@ config = {
 	:SSLCertificate => cert,
 }
 
-DRb.start_service( SERVICE_URI, AuthWrapperObject.new, config )
+# Start the example HomeDirService wrapped in DRb+SSL and wait for it to finish.
+DRb.start_service( SERVICE_URI, HomeDirService.new, config )
 DRb.thread.join
 
 
